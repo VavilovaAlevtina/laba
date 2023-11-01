77f359934e599f18fa33f7d1e031032756c3a8a8
VavilovaAlevtina <146874569+VavilovaAlevtina@users.noreply.github.com>
2023-11-01 22:06:50 +0300

    дополнение кода

---
+++ laba/laba/studentrep.cs
@@ -0,0 +1,60 @@
+﻿using laba;
+
+namespace DataAccess
+{
+    public class StudentsRepository
+    {
+        private List<University> _universities;
+
+        public List<University> Universities => _universities;
+
+        public StudentsRepository(List<University> universities)
+        {
+            _universities = universities;
+        }
+
+        public void AddUniversity(University university)
+        {
+            Universities.Add(university);
+        }
+
+        public void SaveStudentsToFile(string filename, string universityName)
+        {
+            University university = Universities.Find(university => university.Name == universityName);
+
+            if (university == null)
+            {
+                throw new ArgumentException("Университет не найден");
+            }
+
+            using (StreamWriter sw = new StreamWriter(filename))
+            {
+                foreach (Student student in university.Students)
+                {
+                    sw.WriteLine($"{student.Name} {student.Surname} {student.Age} {student.AverageMark}");
+                }
+            }
+        }
+
+        public void LoadStudentsFromFile(string filename, string universityName)
+        {
+            University university = Universities.Find(university => university.Name == universityName);
+
+            if (university == null)
+            {
+                throw new ArgumentException("Университет не найден");
+            }
+
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] studentData = line.Split(' ');
+                    university.AddStudent(new Student(studentData[0], studentData[1], int.Parse(studentData[2]),
+                        int.Parse(studentData[3])));
+                }
+            }
+        }
+    }
+}
